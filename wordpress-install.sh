04b2473ee38c3fc0f0a87e77c2fed84052c2f8be
John Tobin <johntobin@johntobin.ie>
2012-06-16 21:21:34 +0200

    Add support for installing wordpress.

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -8,7 +8,7 @@ die() {
     exit 1
 }
 usage() {
-    die "Usage: $0 [themes|plugins] NAME VERSION"
+    die "Usage: $0 [theme|plugin|wordpress] NAME VERSION"
 }
 
 if [[ "$#" -ne 3 ]]; then
@@ -17,32 +17,48 @@ fi
 TYPE="$1"
 NAME="$2"
 VERSION="$3"
-readonly TYPE NAME VERSION
-if [[ "${TYPE}" != "plugins" && "${TYPE}" != "themes" ]]; then
+readonly TYPE NAME VERSION SUBDIR
+if [[ "${TYPE}" != "plugin" && "${TYPE}" != "theme" \
+        && "${TYPE}" != "wordpress" ]]; then
     usage
 fi
 
+declare -A SUBDIRS SEPARATORS
+SUBDIRS[wordpress]=".."
+SUBDIRS[plugin]="plugins"
+SUBDIRS[theme]="themes"
+SEPARATORS[wordpress]="-"
+SEPARATORS[plugin]="."
+SEPARATORS[theme]="."
+readonly SUBDIRS SEPARATORS
 WORDPRESS_BASE="/var/www/sites/arianetobin.ie/blog"
 readonly WORDPRESS_BASE
-cd "${WORDPRESS_BASE}/wp-content/${TYPE}"
+cd "${WORDPRESS_BASE}/wp-content/${SUBDIRS[${TYPE}]}"
 if [[ -n "$( git status --short )" ]]; then
     die "Uncommitted changes."
 fi
 
 DOWNLOAD_DIR="/home/ariane/wordpress"
-DOWNLOAD_FILE="${NAME}.${VERSION}.zip"
+DOWNLOAD_FILE="${NAME}${SEPARATORS[${TYPE}]}${VERSION}.zip"
 DOWNLOAD_PATH="${DOWNLOAD_DIR}/${DOWNLOAD_FILE}"
-if [[ "${TYPE}" == "themes" ]]; then
-    DOWNLOAD_URL="http://wordpress.org/extend/themes/download/${DOWNLOAD_FILE}"
-elif [[ "${TYPE}" == "plugins" ]]; then
-    DOWNLOAD_URL="http://downloads.wordpress.org/plugin/${DOWNLOAD_FILE}"
-fi
-readonly DOWNLOAD_DIR DOWNLOAD_FILE DOWNLOAD_PATH DOWNLOAD_URL
+declare -A DOWNLOAD_URLS
+DOWNLOAD_URLS[wordpress]="http://wordpress.org/${DOWNLOAD_FILE}"
+DOWNLOAD_URLS[plugin]="http://downloads.wordpress.org/plugin/${DOWNLOAD_FILE}"
+DOWNLOAD_URLS[theme]="http://wordpress.org/extend/themes/download/${DOWNLOAD_FILE}"
+readonly DOWNLOAD_URLS DOWNLOAD_DIR DOWNLOAD_FILE DOWNLOAD_PATH
 if [[ ! -f "${DOWNLOAD_PATH}" ]]; then
-    curl --output "${DOWNLOAD_PATH}" "${DOWNLOAD_URL}" || rm "${DOWNLOAD_PATH}"
+    curl --output "${DOWNLOAD_PATH}" "${DOWNLOAD_URLS[${TYPE}]}" \
+        || rm "${DOWNLOAD_PATH}"
 fi
 
 unzip -q -o "${DOWNLOAD_PATH}"
-git add "${NAME}"
+if [[ "${TYPE}" == "wordpress" ]]; then
+    # Wordpress extracts into a directory named wordpress, and unzip doesn't
+    # have an option to strip a leading directory, so move it with tar, then
+    # delete it.
+    tar cf - -C wordpress . | tar xf -
+    rm -rf wordpress
+fi
+git add .
 (unset LESS; git diff --cached --shortstat)
 git commit --quiet --message "Installed ${NAME} ${VERSION}"
