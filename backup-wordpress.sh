7661c9914c2e89043dc2682bae9b0582993fe062
John Tobin <johntobin@johntobin.ie>
2014-10-12 12:44:39 +0200

    Add /usr/sbin to PATH so tmpreaper will be found.  Misc other cleanups.

--- backup-wordpress.sh
+++ backup-wordpress.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-set -e
+set -e -f -u -o pipefail
+
+PATH="${PATH}:/usr/sbin"
 
 db_password="$( awk -F \' '/DB_PASSWORD/ { print $4 }' \
                 /home/ariane/arianetobin.ie/blog/wp-config.php )"
 date="$( date +%Y-%m-%d-%H-%M )"
-cd ~/wordpress-backups
+cd "${HOME}/wordpress-backups"
 mysqlcheck --analyze --auto-repair --check --repair --silent \
   --password="${db_password}" --databases ariane_blog
 mysqldump --add-drop-table -h localhost -u ariane --password="${db_password}" \
