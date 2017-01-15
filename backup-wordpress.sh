7ddbb48e940d28ea71af88896903aa30f2ab9a45
John Tobin <johntobin@johntobin.ie>
2017-01-15 13:37:46 +0100

    Make backup-wordpress.sh generic.
    Take the path to a Wordpress installation and extract the necessary config info
    from it.

--- backup-wordpress.sh
+++ backup-wordpress.sh
@@ -4,15 +4,32 @@ set -e -f -u -o pipefail
 
 PATH="${PATH}:/usr/sbin"
 
-db_password="$(awk -F \' '/DB_PASSWORD/ { print $4 }' \
-                "${HOME}/arianetobin.ie/wp-config.php")"
-date="$( date +%Y-%m-%d-%H-%M )"
-cd "${HOME}/wordpress-backups"
-for command in --check --analyze; do
-  mysqlcheck "${command}" --auto-repair --silent \
-    --password="${db_password}" --databases ariane_blog
-done
-mysqldump --add-drop-table -h localhost -u ariane --password="${db_password}" \
-        ariane_blog \
-    | bzip2 -c > ariane_blog_"${date}"_sql.bz2
-tmpreaper --mtime 90d .
+wp_option() {
+  local option="$1" config="$2"
+  readonly option config
+
+  awk -F \' "/${option}/"' { print $4 }' "${config}"
+}
+
+main() {
+  local config="$1/wp-config.php" db_password db_name db_user date
+  readonly config
+  db_password="$(wp_option DB_PASSWORD "${config}")"
+  db_name="$(wp_option DB_NAME "${config}")"
+  db_user="$(wp_option DB_USER "${config}")"
+  date="$(date +%Y-%m-%d-%H-%M)"
+  readonly db_password db_name db_user date
+
+  for command in --check --analyze; do
+    mysqlcheck "${command}" --auto-repair --silent \
+      --user="${db_user}" --password="${db_password}" --databases "${db_name}"
+  done
+  mkdir -p "${HOME}/wordpress-backups"
+  cd "${HOME}/wordpress-backups"
+  mysqldump --add-drop-table --host=localhost \
+      --user="${db_user}" --password="${db_password}" --databases "${db_name}" \
+    | bzip2 -c > "${db_name}_${date}"_sql.bz2
+  tmpreaper --mtime 90d .
+}
+
+main "$1"
