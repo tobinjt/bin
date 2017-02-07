06d26d8b578ff9b05bdaa0353e1f8188b764fa8e
John Tobin <johntobin@johntobin.ie>
2017-02-06 22:49:36 -0500

    100% coverage of parse_arguments().

--- python/colx_test.py
+++ python/colx_test.py
@@ -1,6 +1,9 @@
 """Tests for colx."""
 
+import argparse
 import unittest
+
+import mock
 
 import colx
 
@@ -11,13 +14,50 @@ class TestArgumentParsing(unittest.TestCase):
     """Tests for argument parsing."""
 
     tests = [
+        # Very simple test.
         (['1', 'asdf'],
          {
              'columns': [1],
              'filenames': ['asdf'],
+         }),
+        # Negative index.
+        (['--', '-1', 'asdf'],
+         {
+             'columns': [-1],
+             'filenames': ['asdf'],
+         }),
+        # The second digit should be a filename.
+        (['1', 'asdf', '2'],
+         {
+             'columns': [1],
+             'filenames': ['asdf', '2'],
+         }),
+        # Support ranges.
+        (['1:4', 'asdf', '2'],
+         {
+             'columns': [1, 2, 3, 4],
+             'filenames': ['asdf', '2'],
+         }),
+        # Reversed ranges.
+        (['8:4', 'asdf', '2'],
+         {
+             'columns': [8, 7, 6, 5, 4],
+             'filenames': ['asdf', '2'],
+         }),
+        # Negative ranges.
+        (['--', '-4:-1', 'asdf', '2'],
+         {
+             'columns': [-4, -3, -2, -1],
+             'filenames': ['asdf', '2'],
          }),
     ]
     for (args, expected) in tests:
       actual = colx.parse_arguments(args)
       for key in expected.iterkeys():
         self.assertEqual(getattr(actual, key), expected[key])
+
+    # At least one column is required.
+    with mock.patch('argparse.ArgumentParser.error') as mock_error:
+      colx.parse_arguments(['asdf'])
+      mock_error.assert_called_once_with(
+          'At least one COLUMN argument is required.')
