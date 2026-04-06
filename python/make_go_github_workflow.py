63e6d7ea91c87b31b4b37e2da43e1ec8ddf4eb7e
John Tobin <johntobin@johntobin.ie>
2026-04-06 22:05:55 +0100

    feat: generate Go workflows.

---
+++ python/make_go_github_workflow.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Script to generate GitHub Actions workflows for a Golang project."""
+
+import github_workflow_utils
+
+
+def main() -> None:
+    """Parses arguments and generates the workflows."""
+    parser = github_workflow_utils.get_parser(
+        description="Generate GitHub Actions workflows for a Golang project."
+    )
+    args = parser.parse_args(namespace=github_workflow_utils.Args())
+
+    workflows = [
+        # keep-sorted start
+        ("golang_dependabot.template", ".github/dependabot.yml"),
+        ("golang_workflow.template", ".github/workflows/pre-commit.yml"),
+        # keep-sorted end
+    ]
+
+    for template, output_file in workflows:
+        content = github_workflow_utils.generate_workflow(
+            args.program_name,
+            template,
+            __file__,
+            False,
+        )
+        github_workflow_utils.write_workflow(output_file, content)
+
+
+if __name__ == "__main__":
+    main()
