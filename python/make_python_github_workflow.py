d898b1373bfef4976d5fc0a3a7f159bdbdfee2d8
John Tobin <johntobin@johntobin.ie>
2026-05-02 20:49:18 +0100

    feat: add Python workflows, currently just `dependabot`.

---
+++ python/make_python_github_workflow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Script to generate GitHub Actions workflows for a Python project."""
+
+import github_workflow_utils
+
+
+def main() -> None:
+    """Parses arguments and generates the workflows."""
+    workflows = [
+        # keep-sorted start
+        ("dependabot.yml", ".github/dependabot.yml"),
+        # keep-sorted end
+    ]
+
+    github_workflow_utils.run_main(
+        description="Generate GitHub Actions workflows for a Python project.",
+        workflows=workflows,
+        script_file=__file__,
+    )
+
+
+if __name__ == "__main__":
+    main()
